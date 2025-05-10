212ebb09233afdba15bd05f45394ebb42fe56a58
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2025-05-10 13:58:41 -0700

    Exposes the alpha-2 and alpha-3 mapping structure publically

--- src/simpleiso3166/__init__.py
+++ src/simpleiso3166/__init__.py
@@ -4,11 +4,15 @@
 
 from simpleiso3166.base import Country
 from simpleiso3166.base import Subdivision
+from simpleiso3166.generated.mapping import ALPHA2_CODE_TO_COUNTRIES
+from simpleiso3166.generated.mapping import ALPHA3_CODE_TO_COUNTRIES
 from simpleiso3166.generated.types import CountryCodeAlpha2Type
 from simpleiso3166.generated.types import CountryCodeAlpha3Type
 from simpleiso3166.generated.types import SubdivisionTypeType
 
 __all__ = [
+    "ALPHA2_CODE_TO_COUNTRIES",
+    "ALPHA3_CODE_TO_COUNTRIES",
     "Country",
     "CountryCodeAlpha2Type",
     "CountryCodeAlpha3Type",
